ccaffe3cdccdba75c46f79bc911655b35d178e6b
SidahmedYS <sidahmedys10@gmail.com>
2018-09-10 15:15:32 +0100

    annuler l'animation dans le splash screen

--- app/src/main/java/com/washit/washit/welcome.java
+++ app/src/main/java/com/washit/washit/welcome.java
@@ -3,8 +3,6 @@ package com.washit.washit;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +17,9 @@ private ImageView iv;
         setContentView(R.layout.activity_welcome);
         t=(TextView)findViewById(R.id.t);
         iv=(ImageView)findViewById(R.id.iv);
-        Animation a= AnimationUtils.loadAnimation(this,R.anim.myanim);
-        t.startAnimation(a);
-        iv.startAnimation(a);
+        //Animation a= AnimationUtils.loadAnimation(this,R.anim.myanim);
+        //t.startAnimation(a);
+        //iv.startAnimation(a);
         final Intent intent=new Intent(this,LoginActivity.class);
         Thread t=new Thread(){
             public void run(){
